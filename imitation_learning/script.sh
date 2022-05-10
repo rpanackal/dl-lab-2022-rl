5d4409c455100da1cda946c09495e577f50e5fff
Roshin Rajan Panackal <rajanro@tfpool25.informatik.privat>
2022-05-10 05:17:20 +0200

    More runs

--- imitation_learning/script.sh
+++ imitation_learning/script.sh
@@ -2,31 +2,25 @@
 
 echo "Started"
 
-python train.py -hl 1 -l 0.001
+python train.py -hl 15 -l 0.00001
 echo "task1 done"
 
-python train.py -hl 3 -l 0.001
+python train.py -hl 15 -l 0.01
 echo "task2 done"
 
-python train.py -hl 5 -l 0.001
+python train.py -hl 15 -l 0.001
 echo "task3 done"
 
-python train.py -hl 1 -l 0.0001
+python train.py -hl 15 -l 0.0001
 echo "task4 done"
 
-python train.py -hl 3 -l 0.0001
+python train.py -hl 1 -l 0.01
 echo "task5 done"
 
-python train.py -hl 5 -l 0.0001
+python train.py -hl 3 -l 0.01
 echo "task6 done"
 
-python train.py -hl 1 -l 0.00001
+python train.py -hl 5 -l 0.01
 echo "task7 done"
-
-python train.py -hl 3 -l 0.00001
-echo "task8 done"
-
-python train.py -hl 5 -l 0.00001
-echo "task9 done"
 
 
